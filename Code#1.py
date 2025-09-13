3e9a0ade79b4fa3233be22a18a954d71d214a365
PhilULim <philjustin.lim@smc.pshs.edu.ph>
2025-09-13 21:07:11 +0800

    Code#1
    This was made to add three numbers and present the sum.

---
+++ Code#1.py
@@ -0,0 +1,6 @@
+num1 = float(input("Input a number: "))
+num2 = float(input("Input another number: "))
+num3 = float(input("Input another number: "))
+sum =num1+num2+num3
+
+print("Your number is", sum,".")
