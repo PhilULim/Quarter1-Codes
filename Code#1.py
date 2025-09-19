d5efbfa9eaa640ae7c80e72c57509cf404e25cd5
PhilULim <philjustin.lim@smc.pshs.edu.ph>
2025-09-19 19:52:03 +0800

    Delete Code#1.py

--- Code#1.py
+++
@@ -1,6 +0,0 @@
-num1 = float(input("Input a number: "))
-num2 = float(input("Input another number: "))
-num3 = float(input("Input another number: "))
-sum =num1+num2+num3
-
-print("Your number is", sum,".")
