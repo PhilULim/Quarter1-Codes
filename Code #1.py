1d050ae79b856c9432a24333c4fdcd37517f69b9
PhilULim <philjustin.lim@smc.pshs.edu.ph>
2025-09-19 20:00:46 +0800

    Code #1
    This code gets the sum of 3 numbers with decimal places and presents them rounded to the second place value.

---
+++ Code #1.py
@@ -0,0 +1,8 @@
+num1 = float(input("Input a number: "))
+num2 = float(input("Input another number: "))
+num3 = float(input("Input another number: "))
+sum =num1+num2+num3
+
+round_sum = round(sum, 2)
+
+print("Your number is", round_sum,)
